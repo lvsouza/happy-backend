4e4981dab6fe7a7cf3298d3f748e6154d8b1d51e
lvargas <lvargas@gvdasa.com.br>
2021-02-08 21:11:54 -0300

    feat: added cors

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import 'express-async-errors';
 import path from 'path';
+import cors from 'cors';
 
 import { ErrorHandler } from './errors/ErrorHandler';
 import './database/connection';
@@ -10,6 +11,7 @@ import { Routes } from './Routes';
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 
 app.use('/uploads', express.static(path.join(__dirname, '..', 'uploads')));
 
